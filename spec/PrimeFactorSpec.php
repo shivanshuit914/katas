a13b5b74478584be0f479ef850195278c3e160b4
Shivanshu Patel <shivanshu.patel@gohenry.co.uk>
2017-05-25 21:50:20 +0100

    prime factors

--- spec/PrimeFactorSpec.php
+++ spec/PrimeFactorSpec.php
@@ -12,4 +12,34 @@ class PrimeFactorSpec extends ObjectBehavior
     {
         $this->calculate(1)->shouldReturn([]);
     }
+
+    function it_returns_2_for_number_2()
+    {
+        $this->calculate(2)->shouldReturn([2]);
+    }
+
+    function it_returns_3_for_number_3()
+    {
+        $this->calculate(3)->shouldReturn([3]);
+    }
+
+    function it_returns_2_2_for_number_4()
+    {
+        $this->calculate(4)->shouldReturn([2, 2]);
+    }
+
+    function it_returns_2_2_2_for_number_8()
+    {
+        $this->calculate(8)->shouldReturn([2, 2, 2]);
+    }
+
+    function it_returns_11_for_number_11()
+    {
+        $this->calculate(11)->shouldReturn([11]);
+    }
+
+    function it_returns_2_2_2_5_5_5_for_number_1000()
+    {
+        $this->calculate(1000)->shouldReturn([2, 2, 2, 5, 5, 5]);
+    }
 }
--- src/PrimeFactor.php
+++ src/PrimeFactor.php
@@ -2,8 +2,21 @@
 
 class PrimeFactor
 {
+    /**
+     * @param int $number
+     * @return int[]
+     */
     public function calculate(int $number)
     {
-       return [];
+        $primes = [];
+
+        for ($i = 2; $number > 1; $i++) {
+            for (; $number % $i == 0; $number /= $i)
+            {
+                $primes[] = $i;
+            }
+        }
+
+       return $primes;
     }
 }
